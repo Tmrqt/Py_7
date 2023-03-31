4d3e26e3f92d3b76dcfa220ac90309deb06564bf
Tmrqt <120671930+Tmrqt@users.noreply.github.com>
2023-03-31 19:18:25 +0300

    Update Task_34.py

--- Task_34.py
+++ Task_34.py
@@ -1,10 +1,10 @@
 def rifma(poem):
     str = poem.lower().split()
-    f = lambda x: sum(1 for i in x if i in 'аеёиоуыэюя')
+    f = lambda x: sum(1 for i in x if i in "аеёиоуыэюя")
     counter = f(str[0])
     if all([f(i) == counter for i in str]):
-        return 'Парам пам-пам'
-    return 'Пам парам'
+        return "Парам пам-пам"
+    return "Пам парам"
 
 result = rifma(input(""))
 print(result)
